63cafa478ee9d0e1fbb37a910f61d05e7cbfd924
Scott Frasso <ScottFrasso@gmail.com>
2021-09-02 14:58:50 +0200

    Added background jobs.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export async function standupApp(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
+
+  return app;
+}
+
+async function bootstrap() {
+  const app = await standupApp();
   await app.listen(3000);
 }
+
 bootstrap();
